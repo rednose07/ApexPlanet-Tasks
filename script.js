4641aec4f371f03952fb54cd701b60db2f4421bb
Rameet Kumar <161925965+rednose07@users.noreply.github.com>
2025-06-08 23:33:49 +0530

    Add files via upload
    🚀 Web Development Internship Task 1 Completed!
    
    I'm excited to share the task 1, I completed as part of my Web Development Internship at Apex Planet. This opportunity has allowed me to apply my skills in front-end development, enhance my understanding of responsive design, and improve my practical approach to building user-friendly web interfaces.
    
    🔧 Skills Applied:
    ✔ HTML, CSS, JavaScript
    
    A big thanks to Apex Planet for providing such a valuable learning experience. I'm looking forward to more challenges and opportunities to grow as a developer! 🌐💻
    
    #WebDevelopment #InternshipExperience #FrontendDevelopment #ApexPlanet #LearningByDoing #BTechJourney #StudentDeveloper

---
+++ script.js
@@ -0,0 +1,5 @@
+const alertButton = document.getElementById('alertButton');
+
+alertButton.addEventListener('click', function() {
+    alert('Hello! You clicked the button. This is a basic JavaScript interaction!');
+});
